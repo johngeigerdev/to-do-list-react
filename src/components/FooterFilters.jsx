359f35179a7741b9127d140de919850b501ada05
John <jgeiger@Mac.lan>
2025-09-01 16:04:05 -0400

    scss modules created and first commit

---
+++ src/components/FooterFilters.jsx
@@ -0,0 +1,17 @@
+import classes from './FooterFilters.module.scss';
+
+
+function FooterFilter() {
+
+    return (
+    <>
+        <div className={classes.filters}>
+            <div>
+                <p>Footer filters</p>
+            </div>
+        </div>
+    </>
+    )
+  }
+  
+  export default FooterFilter
